2ca6a4a3fd0157d18bc2de716ce05fc16e3db457
HumanHyeon <54471641+HumanHyeon@users.noreply.github.com>
2020-12-27 15:27:55 +0900

    Create programmers_No.11.cpp

---
+++ programmers_No.11.cpp
@@ -0,0 +1,19 @@
+https://programmers.co.kr/learn/courses/30/lessons/12941
+
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int solution(vector<int> A, vector<int> B){
+    int answer = 0;
+
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end(), greater<int>());
+
+    for (int i = 0; i < A.size(); i++)
+        answer += A[i] * B[i];
+
+    return answer;
+}
+
+//sort(B.rbegin(), B.rend());
