c09b17f467afb8d70ae12ab0f4de7372cfe6b622
HumanHyeon <54471641+HumanHyeon@users.noreply.github.com>
2021-01-09 02:42:46 +0900

    Update programmers_No.16.cpp

--- programmers_No.16.cpp
+++ programmers_No.16.cpp
@@ -1,10 +1,10 @@
-https://programmers.co.kr/learn/courses/30/lessons/12919
+https://programmers.co.kr/learn/courses/30/lessons/12917
 
-#include <vector>
-#include <string>
 #include <algorithm>
-using namespace std;
 
-std::string solution(std::vector<std::string> seoul) {
-    return """김서방은 " + std::to_string(find(seoul.begin(), seoul.end(), "Kim") - seoul.begin()) + "에 있다""";
+std::string solution(std::string s) {
+    std::sort(s.rbegin(), s.rend());
+    return s;
 }
+
+//sort(s.begin(), s.end(), greater<char>());
