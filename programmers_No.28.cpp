7d5f64e6dfffb4d89dae01ea3cfada3d6315b44f
HumanHyeon <54471641+HumanHyeon@users.noreply.github.com>
2021-03-14 18:35:11 +0900

    Create programmers_No.28.cpp

---
+++ programmers_No.28.cpp
@@ -0,0 +1,7 @@
+https://programmers.co.kr/learn/courses/30/lessons/12934
+
+long long solution(long long n) {
+	for (long long i = 0; i * i <= n; i++) 
+		if (i * i == n)	return (i + 1) * (i + 1);
+	return -1;
+}
