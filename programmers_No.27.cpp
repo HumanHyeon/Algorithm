88f1a9b608e121bf98f882d1acd91902840baf70
HumanHyeon <54471641+HumanHyeon@users.noreply.github.com>
2021-03-14 18:21:07 +0900

    Create programmers_No.27.cpp

---
+++ programmers_No.27.cpp
@@ -0,0 +1,31 @@
+https://programmers.co.kr/learn/courses/30/lessons/12930
+
+#include <string>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+string change(string s) {
+    for (int i = 0; i < s.size(); i++)
+        if (i % 2) s[i] = tolower(s[i]);
+        else s[i] = toupper(s[i]);
+    return s;
+}
+
+string solution(string s) {
+    vector<string> tmp;
+    stringstream ss(s);
+    string fetch, answer = "";
+    while (ss >> fetch) tmp.push_back(change(fetch));
+
+    bool check = false;
+    int index = 0;
+    for (int i = 0; i < s.size(); i++) 
+        if (s[i] == ' ') answer += " ", check = false;
+        else 
+            if (!check) 
+                answer += tmp[index++], check = true;
+        
+            
+    return answer;
+}
