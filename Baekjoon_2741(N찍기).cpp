10e3d6f5ec7f76fd78842b59908d7dfaf9124936
Park SeHyeon <54471641+HumanHyeon@users.noreply.github.com>
2022-04-11 00:25:22 +0900

    Create Baekjoon_2741(N찍기).cpp

---
+++ Baekjoon_2741(N찍기).cpp
@@ -0,0 +1,14 @@
+https://www.acmicpc.net/problem/2741
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x;
+
+	cin >> x;
+
+	for (int i = 0; i < x; i++) {
+		cout << i + 1 << '\n';
+	}
+}
