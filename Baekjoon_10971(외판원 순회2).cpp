d0f7ce3cd8668e0aedfe198ef58bc394fc40c416
Park SeHyeon <54471641+HumanHyeon@users.noreply.github.com>
2022-04-27 21:26:35 +0900

    Create Baekjoon_10971(외판원 순회2).cpp

---
+++ Baekjoon_10971(외판원 순회2).cpp
@@ -0,0 +1,45 @@
+https://www.acmicpc.net/problem/10971
+
+#include <iostream>
+#include <algorithm>
+#define INF 987654321
+#define MAX 16
+using namespace std;
+
+int N;
+int W[MAX][MAX];
+
+int dp[MAX][1 << MAX];
+
+int tsp(int node, int key) {
+	if (key == (1 << N) - 1) {
+		if (W[node][0] == 0)	return INF;
+		return W[node][0];
+	}
+
+	if (dp[node][key] != INF)	return (dp[node][key]);
+
+	for (int i = 0; i < N; i++) {
+		if (W[node][i] == 0 || key & (1 << i)) continue;
+		dp[node][key] = min(dp[node][key], tsp(i, key | (1 << i)) + W[node][i]);
+	}
+	return (dp[node][key]);
+}
+
+int solution() {
+	for (int i = 0; i < MAX; i++)
+		for (int j = 0; j < (1 << MAX); j++)
+			dp[i][j] = INF;
+	return (tsp(0, 1 << 0));
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);	cout.tie(NULL);
+
+	cin >> N;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			cin >> W[i][j];
+	cout << solution() << endl;
+}
